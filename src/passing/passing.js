117b287d38b90bc24785a824df0e4a57aa897091
Richard Chao <r.chao03@gmail.com>
2017-10-15 20:05:21 -0400

    almost bar ready

--- src/passing/passing.js
+++ src/passing/passing.js
@@ -13,52 +13,102 @@ class Passing extends Component {
   }
 
   componentWillMount() {
-  }
-
-  componentDidMount() {
     const that = this;
     const reg = /TO ([0-9]+-)/;
-    const checkplayer = /TO (.*?) TO/;
-    let num = 0 
-    this.props.filteredData.filter(function(each) {
-      
-      if (each.PlayType == "PASS" && each.Description.match(reg) && each.Description.match(checkplayer)) {
-        let playerName = each.Description.match(checkplayer)[0].split("-")[1].split(" ")[0]
-        let number = each.Description.match(reg)[1].split(" ")[0].split("")[0] + each.Description.match(reg)[1].split(" ")[0].split("")[1]
-        let passLength = each.PassType
-        let touchDown = each.IsTouchdown
-        let redZone = each.YardLineFixed
-        if(playerName == "B.COLEMAN"){
-          console.log(each)
+    const checkplayer = /[0-9]+-([^(\s)]+)[^\.]/mig;
+    let num = 0
+    this.props.filteredData.forEach(function (each) {
+      if (each.IsPass !== "0") {
+        let passLength = each.PassType;
+        let touchDown = each.IsTouchdown;
+        let redZone = each.YardLineFixed;
+        let incomplete = each.IsIncomplete;
+        let playerName = each.Description.match(checkplayer)[1];
+        let newplayerName = that.clean(playerName)
+
+        if (!that.twiceFiltered[newplayerName]) {
+          that.twiceFiltered[newplayerName] = { targets: 0, catches: 0, short: 0, deep: 0, redZone: 0, TD: 0 }
         }
-        if (that.twiceFiltered[playerName]) {
-          that.twiceFiltered[playerName].catches++
-        } else {
-          that.twiceFiltered[playerName] = { catches: 1, number: number, short:0, deep:0, redZone:0, TD:0 }
+        if (that.twiceFiltered[newplayerName]) {
+          that.twiceFiltered[newplayerName].targets++
         }
-        if(touchDown!=="0"){
-          that.twiceFiltered[playerName].TD++
+
+        if (touchDown !== "0") {
+          that.twiceFiltered[newplayerName].TD++
         }
-        if(passLength[0]=="S"){
-          that.twiceFiltered[playerName].short++
-        } else {
-          that.twiceFiltered[playerName].deep++
+        if (passLength[0] == "S" && incomplete == "0") {
+          that.twiceFiltered[newplayerName].short++
+          that.twiceFiltered[newplayerName].catches++
         }
-        if(redZone<=21){
-          that.twiceFiltered[playerName].redZone++
+        if (passLength[0] == "D" && incomplete == "0") {
+          that.twiceFiltered[newplayerName].deep++
+          that.twiceFiltered[newplayerName].catches++
+        }
+        if (redZone <= 21) {
+          that.twiceFiltered[newplayerName].redZone++
         }
       }
+
     })
     console.log("asdf", this.twiceFiltered)
   }
 
-  componentDidUpdate() {}
+  componentDidUpdate() {
+    console.log("componentn did update")
+  }
 
-  render() {
+  componentDidMount() {
+   
+  }
+
+  createBarGraph(){
+    const that = this;
+
+    let arrayOfData =  d3.entries(that.twiceFiltered)
+   arrayOfData.forEach((d)=>{
+    console.log("name", d.key)
+    console.log("catches", d.value.catches)
+   })
+  }
+
+
+  clean(word) {
+    const that = this;
+    if(word){
+    let cleaning = word.split("")
+    if (cleaning[cleaning.length - 1] == " ") {
+      cleaning.pop();
+      that.clean(cleaning.join(""))
+    }
+    if (cleaning[cleaning.length - 1] === ".") {
+      cleaning.pop();
+      that.clean(cleaning.join(""))
+
+    }
+    if (cleaning[cleaning.length - 1] == ")") {
+      cleaning.pop();
+      that.clean(cleaning.join(""))
+
+    }
+    if (cleaning[cleaning.length - 1] == ";") {
+      cleaning.pop();
+      that.clean(cleaning.join(""))
+    }
+    return (cleaning.join(""))
+    } else {
+      return
+    }
+  }
+  
+    render() {
+      this.createBarGraph()
 
     return (
       <div>
 Here is phase 2
+<div className="barGraphWrapper">
+<div className="barGraph"></div>
+</div>
       </div>
     );
   }
